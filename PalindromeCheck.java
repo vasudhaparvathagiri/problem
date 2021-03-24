921af603f04a386871279f86f8e10e6d87ab090e
vasudhaparvathagiri <68888890+vasudhaparvathagiri@users.noreply.github.com>
2021-03-24 17:20:10 -0500

    Update PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -8,23 +8,23 @@ public class PalindromeCheck {
 		// TODO Auto-generated method stub
 		System.out.println("Enter the String to find its longest palindromic substring");
 		Scanner sc = new Scanner(System.in);
-		String _s_ = sc.next();
-		int _s_Length = _s_.length();
+		String s = sc.next();
+		int sLength = s.length();
 		int maxLength = 1;
-		if(_s_.length()<=1 || _s_ == null) {
-			System.out.println("The longest palindromic substring is "+_s_);
+		if(s.length()<=1 || s == null) {
+			System.out.println("The longest palindromic substring is "+s);
 			System.exit(0);
 		}
-		boolean[][] table = new boolean[_s_Length][_s_Length];
+		boolean[][] table = new boolean[sLength][sLength];
 		String palindromeLongSub = null;
-		 for(int k=0;k<_s_.length();k++){
-		        for(int i=0;i<_s_Length-k;i++){
+		 for(int k=0;k<s.length();k++){
+		        for(int i=0;i<sLength-k;i++){
 		            int j= i+k;
-		            if(_s_.charAt(i)==_s_.charAt(j)&&(j-i<=2||table[i+1][j-1])){
+		            if(s.charAt(i)==s.charAt(j)&&(j-i<=2||table[i+1][j-1])){
 		                table[i][j]=true;
 		                if(j-i+1>maxLength){
 		                   maxLength = j-i+1; 
-		                   palindromeLongSub = _s_.substring(i, j+1);
+		                   palindromeLongSub = s.substring(i, j+1);
 		                }
 		            }
 		        }
